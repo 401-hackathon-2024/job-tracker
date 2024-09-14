136337fb2924b46bac0815d921cc921206f980aa
Sundance636 <s.clement636@gmail.com>
2024-09-14 16:44:36 -0600

    Readded UI changes

--- src-ext/src/App.tsx
+++ src-ext/src/App.tsx
@@ -3,23 +3,13 @@ import React from 'react';
 const App: React.FC = () => {
     const handleAutofill = () => {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            if (tabs[0].id) {
-                chrome.tabs.sendMessage(tabs[0].id, { action: "autofillForm" }, (response) => {
-                    if (chrome.runtime.lastError) {
-                        console.error("Error sending message:", chrome.runtime.lastError.message);
-                    } else if (response && response.status) {
-                        console.log("Response status:", response.status);
-                    } else {
-                        console.error("No response or status received");
-                    }
-                });
-            } else {
-                console.error("No active tab found");
-            }
+            chrome.tabs.sendMessage(tabs[0].id!, { action: "autofillForm" }, (response) => {
+                console.log(response.status);
+            });
         });
     };
 
-    return (<div id="container">
+    return (<div id="container" style={{backgroundColor: ''}}>
     <h2>Auto Job Application Filler</h2>
 
     {/* Dropdown List should have the autofill profiles */}
