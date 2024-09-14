208e6df3d89e24cc2d211478ba2600230bff9244
Alex <al.willet02@gmail.com>
2024-09-14 15:31:22 -0600

    Admin stuff registered for db

--- src-django/src/config/admin.py
+++ src-django/src/config/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from website.models import Resume, User, Education, Project, Language, Url, Address
 
 # Register your models here.
+
+admin.site.register(Resume)
+admin.site.register(User)
+admin.site.register(Education)
+admin.site.register(Project)
+admin.site.register(Language)
+admin.site.register(Url)
+admin.site.register(Address)
